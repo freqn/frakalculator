956eea0f8803b6fae0a452fff74d86bc9cf69560
Chip Eyler <ceyler@gmail.com>
2015-03-29 15:35:26 -0400

    Angularized

---
+++ js/app.js
@@ -0,0 +1,22 @@
+window.addEventListener("load",function() {
+	setTimeout(function(){
+		window.scrollTo(0, 1);
+	}, 0);
+});
+
+(function(){
+  var app = angular.module('AngCalc',[]);
+  app.controller('CalcCtrl', function($scope){
+    $scope.cVal = {
+      quant: '',
+      itemCost: '',
+      soldPrice: '',
+      shipping: ''
+    };
+  });
+  $(".fa-info").on('click', function(event){
+    event.preventDefault();
+    alert("A simple profit calulator.\n\nDownload this as an app by adding to your homescreen.\n\nMore features coming soon.\n\nCopyright 2015 Chip Eyler\nAll Rights Reserved.");
+
+  });
+})();
